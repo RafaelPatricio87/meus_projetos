9d9c4a324f9c5d1f709b795dc78fe749270d55bb
Rafael <rafaelpatricio2002@gmail.com>
2023-12-03 03:12:19 -0300

    adicionado o score

--- mario__jump/js/script.js
+++ mario__jump/js/script.js
@@ -1,6 +1,9 @@
 
 const mario = document.querySelector('.mario')
 const pipe = document.querySelector('.pipe');
+const scoreDisplay = document.querySelector('.score');
+
+let score = 0;
 
 const jump = () => {
   mario.classList.add('jump')
@@ -10,13 +13,17 @@ const jump = () => {
  },500)  
 }
 
+
+const updateScore = () => {
+  score += 1;
+  scoreDisplay.textContent = `Pontuação: ${score}`;
+};
 const loop = setInterval(() => {
   
   const pipePosition =  pipe.offsetLeft;
   const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','')
- 
   if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
-
+    
     pipe.style.animation = 'none'
     pipe.style.left = `${pipePosition}px`;
 
@@ -28,9 +35,18 @@ const loop = setInterval(() => {
     mario.style.margin = '50px'
 
     clearInterval(loop)
+  }else if (pipePosition < -50) {
+    // O cano ultrapassou o Mario com sucesso
+    updateScore();
+       // Reinicia a animação do cano
+    const newPipePosition = window.innerWidth; // Posiciona o cano fora da tela à direita
+    pipe.style.left = `${newPipePosition}px`;
+  } else {
+    pipe.style.left = `${pipePosition - 5}px`; // Move o cano para a esquerda
   }
-
+  
 })
+
 
 
 document.addEventListener('keydown', jump);
