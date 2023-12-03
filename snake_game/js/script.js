9b8647e1a6e99fa5dd27953c728e32e951a782ab
Rafael <rafaelpatricio2002@gmail.com>
2023-12-03 04:38:52 -0300

    projeto snake game

---
+++ snake_game/js/script.js
@@ -0,0 +1,4 @@
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
+
+// Como criar o jogo da cobrinha com JavaScript e HTML | JavaScript para iniciantes - Tutorial
